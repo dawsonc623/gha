4dc4bf16ad90ca1cab1cabd8207076e6f8c8e294
Cyle Ven Dawson <cyle.ven.dawson@varsitytutors.com>
2022-05-06 16:14:41 -0500

    fix: only print timestamp of last successful run

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-throw new Error("trigger error!");
-// console.log(`Last build timestamp is ${process.env.LAST_BUILD_TIMESTAMP}`);
+console.log(`Last successful build timestamp is ${process.env.LAST_BUILD_TIMESTAMP}`);
